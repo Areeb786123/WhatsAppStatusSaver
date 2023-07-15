b3adb0e638ba0a7d89af6e8a405a311a7cc8eb8e
areeb786 <areebrockzz786@gmail.com>
2023-07-15 21:05:18 +0530

    chi chi

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,15 @@ Sapp/src/main/java/com/areeb/whatsappstatussaver/ui/home/fragments/VideoFragment
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
 ƒ
 Sapp/src/main/java/com/areeb/whatsappstatussaver/ui/home/adapter/ViewPagerAdapter.kt,e/4/e46fbe15291081d7a7c13f69d6ca122b316a9339
+Š
+Zapp/src/main/java/com/areeb/whatsappstatussaver/ui/DetailScreen/activity/DetailActivity.kt,7/8/78d635185a982c4b08fe7f7c5cb2dbc2157fc9db
+x
+Happ/src/main/java/com/areeb/whatsappstatussaver/ui/common/OnItemClick.kt,6/9/69b9616991cc129e6237ea361c4c5d0435439d31
+„
+Tapp/src/main/java/com/areeb/whatsappstatussaver/ui/home/fragments/PhotosFragments.kt,6/e/6e501223b0a028f9aa16e41f370584420e5eb2e9
+…
+Uapp/src/main/java/com/areeb/whatsappstatussaver/ui/home/adapter/ImageStatusAdapter.kt,0/c/0c89d2790c96665f8ab35533f55e7cf8319f7920
+{
+Kapp/src/main/java/com/areeb/whatsappstatussaver/ui/splash/SplashActivity.kt,4/f/4fd3468026e40d03ce800ddfa94031af2ad8cd4f
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
--- app/src/main/java/com/areeb/whatsappstatussaver/ui/DetailScreen/fragments/DetailFragment.kt
+++ app/src/main/java/com/areeb/whatsappstatussaver/ui/DetailScreen/fragments/DetailFragment.kt
@@ -1,18 +1,32 @@
 package com.areeb.whatsappstatussaver.ui.DetailScreen.fragments
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.areeb.whatsappstatussaver.databinding.FragmentDetailBinding
+import com.areeb.whatsappstatussaver.ui.base.fragments.BaseFragments
 import com.areeb.whatsappstatussaver.utils.constants.Constants.TARGET_DIRECTORY.SHARING.Companion.FRAGMENT_IMAGE_URI
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
 @AndroidEntryPoint
-class DetailFragment : Fragment() {
+class DetailFragment : BaseFragments(), View.OnClickListener {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
@@ -31,11 +45,102 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getData(arguments)
+        onItemClick()
+    }
+
+    private fun downloadImage() {
+        val drawable = binding.detailImage.drawable as? BitmapDrawable
+        val bitmap = drawable?.bitmap
+
+        if (bitmap != null) {
+            saveImageToExternalStorage(bitmap)
+        }
     }
 
     private fun getData(arguments: Bundle?) {
-        Log.e("aa", arguments?.getString(FRAGMENT_IMAGE_URI).toString())
         imageUrl = arguments?.getString(FRAGMENT_IMAGE_URI).toString()
         Glide.with(requireContext()).load(imageUrl).into(binding.detailImage)
+    }
+
+    private fun onItemClick() {
+        binding.btnDownloadImageStatus.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            binding.btnDownloadImageStatus.id -> {
+                // Request external storage permission if not granted
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        PERMISSION_REQUEST_CODE,
+                    )
+                }
+            }
+        }
+    }
+
+    private fun saveImageToExternalStorage(bitmap: Bitmap) {
+        val fileName = "image.jpg"
+        val directory =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+
+        val file = File(directory, fileName)
+
+        try {
+            FileOutputStream(file).use { outputStream ->
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun saveBitmapFile() {
+        val drawable = binding.detailImage.drawable as? BitmapDrawable
+        val bitmap = drawable?.bitmap
+
+        val displayName = "my_image.png" // Name of the image file
+        val mimeType = "image/png" // MIME type of the image file
+
+        val imageCollection =
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, displayName)
+            put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+        }
+
+        val resolver = requireContext().contentResolver
+        var outputStream: OutputStream? = null
+
+        try {
+            val imageUri = resolver.insert(imageCollection, imageDetails)
+            outputStream = imageUri?.let { resolver.openOutputStream(it) }
+            bitmap?.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            Toast.makeText(requireContext(), "Image saved successfully", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), "Failed to save image", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        } finally {
+            outputStream?.close()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            saveBitmapFile()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
