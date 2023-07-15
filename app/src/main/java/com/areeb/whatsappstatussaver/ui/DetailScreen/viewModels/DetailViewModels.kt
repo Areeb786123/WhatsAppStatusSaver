dfe55fb3d025cf0a2cbdb10346667e3d4c913650
areeb786 <areebrockzz786@gmail.com>
2023-07-15 22:55:53 +0530

    ss

--- app/src/main/java/com/areeb/whatsappstatussaver/ui/DetailScreen/viewModels/DetailViewModels.kt
+++ app/src/main/java/com/areeb/whatsappstatussaver/ui/DetailScreen/viewModels/DetailViewModels.kt
@@ -10,7 +10,9 @@ import android.os.Build
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import java.io.IOException
 import javax.inject.Inject
 
@@ -18,44 +20,47 @@ import javax.inject.Inject
 class DetailViewModels @Inject constructor(private val context: Context) : ViewModel() {
 
     suspend fun saveImage(bitmap: Bitmap, requireActivity: Activity) {
-        val contentResolver: ContentResolver = requireActivity.applicationContext.contentResolver
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, "image.jpg")
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-        }
-
-        var imageUri: String? = null
-
-        try {
-            val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
-            } else {
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        viewModelScope.launch {
+            val contentResolver: ContentResolver =
+                requireActivity.applicationContext.contentResolver
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, "image.jpg")
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
             }
 
-            val imageUri = contentResolver.insert(imageCollection, contentValues)
-            if (imageUri != null) {
-                contentResolver.openOutputStream(imageUri)?.use { outputStream ->
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            var imageUri: String? = null
+
+            try {
+                val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+                } else {
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
                 }
 
-                Toast.makeText(
-                    context,
-                    "Image saved successfully.",
-                    Toast.LENGTH_SHORT,
-                ).show()
-            } else {
-                Toast.makeText(
-                    context,
-                    "Failed to save the image.",
-                    Toast.LENGTH_SHORT,
-                ).show()
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            imageUri?.let { uri ->
-                contentResolver.delete(Uri.parse(uri), null, null)
+                val imageUri = contentResolver.insert(imageCollection, contentValues)
+                if (imageUri != null) {
+                    contentResolver.openOutputStream(imageUri)?.use { outputStream ->
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                    }
+
+                    Toast.makeText(
+                        context,
+                        "Image saved successfully.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Failed to save the image.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                imageUri?.let { uri ->
+                    contentResolver.delete(Uri.parse(uri), null, null)
+                }
             }
         }
     }
